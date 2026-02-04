3db4b2b62e0d79b58f6d8733f862fb8a6ed1d450
marcieldias6 <marcieldias6@gmail.com>
2026-02-04 02:28:12 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,20 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
+  const env = loadEnv(mode, '.', '');
+  return {
+    base: './', // <--- ADICIONE ESTA LINHA AQUI
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    // ... restante do código
+//export default defineConfig(({ mode }) => {
+  //  const env = loadEnv(mode, '.', '');
+   // return {
+     // server: {
+       // port: 3000,
+        //host: '0.0.0.0',//
       },
       plugins: [react()],
       define: {
